6f7ccc1e0a80c9c0644f6e92c00e7509c2f54c51
realprogrammer5000 <me@programmer050.com>
2021-01-14 18:21:00 -0500

    Source control is overrated

---
+++ src/main/java/com/programmer5000/rompplugin/ChestListener.java
@@ -0,0 +1,208 @@
+package com.programmer5000.rompplugin;
+
+import java.util.*;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Chest;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.inventory.*;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.util.permissions.BroadcastPermissions;
+import sun.awt.SunToolkit;
+
+class RecipeShape {
+  private ItemStack[] items;
+  public RecipeShape(ItemStack[] items) {
+    this.items = items;
+  }
+  public int hashCode() {
+    return Objects.hash(items);
+  }
+  public boolean equals(Object o) {
+    return o != null && o.hashCode() == hashCode();
+  }
+}
+
+public class ChestListener implements Listener {
+//  InventoryInteractEvent
+//  @EventHandler
+//  public void onPlayerJoin(PlayerJoinEvent event) {
+//    Bukkit.broadcastMessage("Welcome to the server!");
+//  }
+
+//  @EventHandler
+//  public void onInventoryInteract(InventoryInteractEvent event) {
+//    Bukkit.getLogger().info("inventory interact!!!");
+//  }
+
+  final int[] topRow = {0, 1, 2};
+  final int[] middleRow = {9, 10, 11};
+  final int[] bottomRow = {18, 19, 20};
+  final int[] allRows = {0, 1, 2, 9, 10, 11, 18, 19, 20};
+
+  @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+  public void onInventoryClick(final InventoryClickEvent event){
+    boolean isChest = event.getInventory().getType() == InventoryType.CHEST;
+    if(!isChest) return;
+    if(!checkInventoryIsSpecialChest(event.getInventory())) return;
+
+    boolean slotIsContainer = event.getSlotType() == InventoryType.SlotType.CONTAINER;
+    boolean clickedChest = (event.getClickedInventory() != null && event.getClickedInventory().getType() == InventoryType.CHEST);
+
+    boolean otherInventoryMove = event.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY || event.getAction() == InventoryAction.COLLECT_TO_CURSOR;
+
+    int slot = event.getSlot();
+    boolean isOkSlot = contains(allRows, slot);
+
+    if (isChest && ((slotIsContainer && clickedChest && !isOkSlot) || otherInventoryMove)) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler(priority = EventPriority.HIGHEST)
+  public void onInventoryDrag(final InventoryDragEvent event){
+    boolean isChest = event.getInventory().getType() == InventoryType.CHEST;
+    if(!isChest) return;
+    if(!checkInventoryIsSpecialChest(event.getInventory())) return;
+
+    boolean clickedChest = event.getInventory() != null && event.getInventory().getType() == InventoryType.CHEST;
+    Set<Integer> slots = event.getInventorySlots();
+    boolean isOkSlot = true;
+
+    for(Integer i : slots){
+      if (!contains(allRows, i)) {
+        isOkSlot = false;
+        break;
+      }
+    }
+
+    if ((isChest && clickedChest) && !isOkSlot) {
+      event.setCancelled(true);
+    }
+  }
+
+  private boolean checkInventoryIsSpecialChest(Inventory inventory){
+      return inventory.getType() == InventoryType.CHEST && ((Chest) inventory.getHolder()).getBlock().getType() == Material.TRAPPED_CHEST;
+  }
+
+
+  @EventHandler(priority = EventPriority.HIGHEST)
+  public void onInventoryMove(final InventoryMoveItemEvent event){
+    Inventory sourceInventory = event.getSource();
+    Inventory destInventory = event.getDestination();
+
+    boolean originatesFromChest = sourceInventory.getType() == InventoryType.CHEST;
+    boolean goesToChest = destInventory.getType() == InventoryType.CHEST;
+
+    boolean originatesFromHopper = sourceInventory.getType() == InventoryType.HOPPER;
+    boolean goesToHopper = destInventory.getType() == InventoryType.HOPPER;
+
+    boolean chestToHopper = originatesFromChest && goesToHopper;
+    boolean hopperToChest = originatesFromHopper && goesToChest;
+
+    if(!chestToHopper && !hopperToChest) return;
+
+    if(hopperToChest) {
+      if(!checkInventoryIsSpecialChest(destInventory)) return;
+
+      ItemStack stack = event.getItem().clone();
+      stack.setAmount(1);
+
+
+      ItemStack[] contents = destInventory.getContents();
+
+      int smallestStackOfThisItem = 999999;
+      int smallestStackOfThisItemPos = -1;
+      for (int i = 0; i < contents.length; i++) {
+        // this slot is empty, so it's not part of the template
+        if (contents[i] == null) continue;
+
+        boolean isValidSlot = contains(allRows, i);
+        boolean canStack = contents[i].isSimilar(stack);
+        boolean isSmallerStack = contents[i].getAmount() < smallestStackOfThisItem;
+        if (isValidSlot && canStack && isSmallerStack) {
+          smallestStackOfThisItemPos = i;
+          smallestStackOfThisItem = contents[i].getAmount();
+        }
+      }
+
+      if (smallestStackOfThisItemPos != -1 && smallestStackOfThisItem < stack.getMaxStackSize()) {
+        ItemStack newStackHere = contents[smallestStackOfThisItemPos].clone();
+        newStackHere.setAmount(newStackHere.getAmount() + 1);
+        destInventory.setItem(smallestStackOfThisItemPos, newStackHere);
+
+        event.getItem().setAmount(0);
+      } else {
+        event.setCancelled(true);
+      }
+
+      destInventory.getViewers().forEach(humanEntity -> {
+        if(humanEntity instanceof Player){
+          ((Player) humanEntity).updateInventory();
+        }
+      });
+    }
+    if(chestToHopper){
+      if(!checkInventoryIsSpecialChest(sourceInventory)) return;
+
+      ItemStack[] stack = new ItemStack[9];
+      int pos = 0;
+      for(int i : allRows){
+        ItemStack thisItem = sourceInventory.getItem(i);
+        if(thisItem != null) {
+          thisItem = thisItem.clone();
+          boolean isAir = thisItem.getType() == Material.AIR;
+
+          boolean lastItem = thisItem.getAmount() == 1 && !SpigotPlugin.getInstance().getConfig().getBoolean("enableRecipeEmpty");
+
+          if (lastItem && !isAir && pos != 0) {
+            event.setCancelled(true);
+            return;
+          }
+
+          thisItem.setAmount(1);
+          stack[pos] = thisItem;
+        }else {
+          stack[pos] = null;
+        }
+
+        pos++;
+      }
+
+      RecipeShape shape = new RecipeShape(stack);
+      ShapedRecipe shapedCrafted = SpigotPlugin.getInstance().getShapedRecipeMap().get(shape);
+      ShapelessRecipe shapelessCrafted = SpigotPlugin.getInstance().getShapelessRecipeMap().get(Arrays.asList(stack));
+
+      ItemStack outItem = null;
+      if(shapelessCrafted != null) outItem = shapelessCrafted.getResult();
+      if(shapedCrafted != null) outItem = shapedCrafted.getResult();
+
+      if(outItem != null) {
+        event.setItem(outItem);
+
+        for(int i : allRows){
+          ItemStack usedStack = sourceInventory.getItem(i);
+          if(usedStack != null) usedStack.setAmount(usedStack.getAmount() - 1);
+          sourceInventory.setItem(i, usedStack);
+        }
+      }else{
+        event.setCancelled(true);
+      }
+    }
+  }
+
+  private boolean contains(final int[] array, final int key){
+    for(int i : array){
+      if(i == key){
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ src/main/java/com/programmer5000/rompplugin/SpigotPlugin.java
@@ -0,0 +1,94 @@
+package com.programmer5000.rompplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class SpigotPlugin extends JavaPlugin {
+    private static SpigotPlugin instance;
+    public static final SpigotPlugin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onDisable() {
+        // Don't log disabling, Spigot does that for you automatically!
+    }
+
+    public Map<RecipeShape, ShapedRecipe> getShapedRecipeMap() {
+        return shapedRecipeMap;
+    }
+
+    public Map<List<ItemStack>, ShapelessRecipe> getShapelessRecipeMap() {
+        return shapelessRecipeMap;
+    }
+
+    private Map<RecipeShape, ShapedRecipe> shapedRecipeMap;
+    private Map<List<ItemStack>, ShapelessRecipe> shapelessRecipeMap;
+
+    private ItemStack[] shapedRecipeToItemStack(ShapedRecipe shapedRecipe){
+        ItemStack[] items = new ItemStack[9];
+
+        int i = 0;
+        int row = 0;
+        for (String s : shapedRecipe.getShape()) {
+            i = 0;
+            for (char c : s.toCharArray()) {
+                items[i + row * 3] = shapedRecipe.getIngredientMap().get(c);
+                i++;
+            }
+            row++;
+        }
+
+        return items;
+    }
+
+
+    @Override
+    public void onEnable() {
+        FileConfiguration config = this.getConfig();
+        config.addDefault("enableRecipeEmpty", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+
+
+        instance = this;
+
+        shapedRecipeMap = new HashMap();
+        shapelessRecipeMap = new HashMap();
+
+        Bukkit.getLogger().info("calculating recipe maps...");
+        Iterator<Recipe> iterator = Bukkit.recipeIterator();
+        while(iterator.hasNext()){
+            Recipe recipe = iterator.next();
+            if(recipe instanceof ShapedRecipe){
+                ShapedRecipe shapedRecipe = (ShapedRecipe) recipe;
+                RecipeShape recipeShape = new RecipeShape(shapedRecipeToItemStack(shapedRecipe));
+
+                shapedRecipeMap.put(recipeShape, shapedRecipe);
+            }
+            if(recipe instanceof ShapelessRecipe){
+                ShapelessRecipe shapelessRecipe = (ShapelessRecipe) recipe;
+                shapelessRecipeMap.put(shapelessRecipe.getIngredientList(), shapelessRecipe);
+            }
+        }
+        Bukkit.getLogger().info("calculated recipe maps!");
+
+        // Don't log enabling, Spigot does that for you automatically!
+
+        // Commands enabled with following method must have entries in plugin.yml
+        getCommand("example").setExecutor(new ExampleCommand(this));
+
+        getServer().getPluginManager().registerEvents(new ChestListener(), this);
+    }
+}
