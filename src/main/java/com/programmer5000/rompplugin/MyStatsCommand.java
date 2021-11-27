0f640643229489b83b8f12071e568478c837a3b4
realprogrammer5000 <me@programmer5000.com>
2021-11-26 19:19:55 -0600

    Add text search to command

--- src/main/java/com/programmer5000/rompplugin/MyStatsCommand.java
+++ src/main/java/com/programmer5000/rompplugin/MyStatsCommand.java
@@ -1,5 +1,6 @@
 package com.programmer5000.rompplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,11 +9,16 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class MyStatsCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
     if (sender instanceof Player) {
+      Logger logger = Bukkit.getLogger();
+
       Player player = (Player) sender;
 
 
@@ -22,18 +28,29 @@ public class MyStatsCommand implements CommandExecutor {
         player.sendMessage("Cleared your scoreboard.");
 
         return true;
-      }
+      }else if(args.length > 1 && args[0].equalsIgnoreCase("display")) {
+        String searchStr = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+        if(searchStr.startsWith("\"") && searchStr.endsWith("\"")){
+          searchStr = searchStr.substring(1, searchStr.length() - 1);
+        }
 
 //      Statistic stat = Statistic.ENTITY_KILLED_BY;
 //      Object statEntityOrMaterialOrNull = EntityType.ZOMBIE;
 //
 //      StatsBoard statsBoard = StatsBoardManager.getInstance().getBoard(stat, statEntityOrMaterialOrNull);
 
-      StatsBoard statsBoard = StatsBoardManager.getInstance().getBoard("Times Killed by Zombie");
-      statsBoard.addPlayer(player);
+        StatsBoard statsBoard = StatsBoardManager.getInstance().getBoard(searchStr);
 
+        if(statsBoard == null){
+          player.sendMessage("Could not find scoreboard " + searchStr);
+        }else {
+          statsBoard.addPlayer(player);
+        }
 
-      return true;
+        return true;
+      }else{
+        return false;
+      }
     }else{
       return false;
     }
