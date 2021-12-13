b982999aab394456a45faa12d2224491183810fb
realprogrammer5000 <me@programmer5000.com>
2021-12-12 18:04:56 -0600

    A small amount of trolling

--- src/main/java/com/programmer5000/rompplugin/MotdListener.java
+++ src/main/java/com/programmer5000/rompplugin/MotdListener.java
@@ -5,18 +5,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
+import org.bukkit.util.CachedServerIcon;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.net.URL;
+import java.util.*;
 
 public class MotdListener implements Listener {
   @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
   public void motdListener(final ServerListPingEvent event) {
-
     try {
       File files = new File(SpigotPlugin.getInstance().getDataFolder().getAbsolutePath() + "/motds.txt");
       Scanner scanner = new Scanner(files);
@@ -26,10 +24,21 @@ public class MotdListener implements Listener {
         if (!line.isEmpty()) options.add(line);
       }
 
-      String motdBase = SpigotPlugin.getInstance().getServer().getMotd();
-      String secondLine = options.get(new Random().nextInt(options.size()));
-      // make message white
-      event.setMotd(motdBase + "\n\u00A7f" + secondLine);
+      if(new Random().nextInt(100) > 98){
+        try {
+          File file = new File(SpigotPlugin.getInstance().getDataFolder().getAbsolutePath() + "/troll.png");
+          event.setServerIcon(Bukkit.loadServerIcon(file));
+        }catch(Exception e){
+          e.printStackTrace();
+        }
+
+        event.setMotd("Your IP address is " + event.getAddress().getHostAddress() + "\nI am rapidly approaching your location");
+      }else {
+        String motdBase = SpigotPlugin.getInstance().getServer().getMotd();
+        String secondLine = options.get(new Random().nextInt(options.size()));
+        // make message white
+        event.setMotd(motdBase + "\n\u00A7f" + secondLine);
+      }
     } catch (FileNotFoundException e) {
       String errorStr = "Could not find file!\nAdd motds.txt to ./plugins/" + SpigotPlugin.getInstance().getDataFolder().getName();
       Bukkit.getLogger().info(errorStr + e);
